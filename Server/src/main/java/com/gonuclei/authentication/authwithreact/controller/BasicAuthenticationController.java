0f74abb28221a729c2d771be3dd5c49156ee8cdf
Piyush Mehta <me@piyushmehta.com>
2020-01-27 17:43:36 +0530

    authentication controller and a hardcoded bean

---
+++ Server/src/main/java/com/gonuclei/authentication/authwithreact/controller/BasicAuthenticationController.java
@@ -0,0 +1,16 @@
+package com.gonuclei.authentication.authwithreact.controller;
+
+import com.gonuclei.authentication.authwithreact.model.AuthenticationBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins={ "http://localhost:3000", "http://localhost:4200" })
+@RestController
+public class BasicAuthenticationController {
+    @GetMapping(path = "/basicauth")
+    public AuthenticationBean authenticate() {
+        //throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+        return new AuthenticationBean("You are authenticated");
+    }
+}
---
+++ Server/src/main/java/com/gonuclei/authentication/authwithreact/model/AuthenticationBean.java
@@ -0,0 +1,20 @@
+package com.gonuclei.authentication.authwithreact.model;
+
+public class AuthenticationBean {
+    private String message;
+
+    public AuthenticationBean() {
+    }
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
