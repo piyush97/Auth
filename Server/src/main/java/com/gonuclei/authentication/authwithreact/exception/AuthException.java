32ba06834e5d54c7f252b6af024f811b9c5490dd
Piyush Mehta <me@piyushmehta.com>
2020-01-27 19:28:19 +0530

    exception

---
+++ Server/src/main/java/com/gonuclei/authentication/authwithreact/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.gonuclei.authentication.authwithreact.exception;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
