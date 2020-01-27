4dc708ccc97c5bac9d72304bba524a54b9fbdaff
Piyush Mehta <me@piyushmehta.com>
2020-01-27 16:12:50 +0530

    Init

---
+++ src/main/java/com/gonuclei/authentication/authwithreact/AuthwithreactApplication.java
@@ -0,0 +1,13 @@
+package com.gonuclei.authentication.authwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthwithreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthwithreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonuclei/authentication/authwithreact/AuthwithreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonuclei.authentication.authwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthwithreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
